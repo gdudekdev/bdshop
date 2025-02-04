b07d58e2fcd0b23f8329c884eed549c02cffa5b5
gdudek <gdudek.dev@gmail.com>
2025-02-04 11:18:08 +0100

    chore/Prevention faille XSS dans index.php

--- admin/include/function.php
+++ admin/include/function.php
@@ -1,4 +1,15 @@
 <?php
+
+/**
+ * Redirects the user to the specified path.
+ *
+ * This function sends an HTTP header to the client to redirect them to the given path
+ * and then terminates the script execution.
+ *
+ * @param string $path The path to redirect to. This can be a relative or absolute URL.
+ *
+ * @return void
+ */
 function redirect($path){
     header('Location:' . $path);
     exit();
