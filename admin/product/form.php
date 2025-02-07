2a897102f8dcb67b5860c56ab17a687ec018db38
gdudek <gdudek.dev@gmail.com>
2025-02-07 11:31:50 +0100

    /feat Modification d'un champ présent dans la base de données

--- admin/product/form.php
+++ admin/product/form.php
@@ -2,6 +2,24 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/function.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/protect.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/connect.php";
+
+// Initialisation du formulaire (nécessaire pour que l'on puisse ajouter un produit et le modifier avec le même formulaire)
+$product_serie = "";
+$product_name = "";
+$product_id = 0;
+$product_date=date("Y-m-d"); // Dans le cas où on a un champ date
+
+if (isset($_GET["id"]) && is_numeric($_GET["id"])){	
+	$stmt= $db -> prepare ("SELECT * FROM table_product WHERE product_id = :product_id");
+	$stmt -> bindValue(":product_id", $_GET["id"]);
+	$stmt -> execute();
+
+	if($row = $stmt -> fetch()){
+		$product_serie = $row["product_serie"];
+		$product_name = $row["product_name"];
+		$product_id = $row["product_id"];
+	};
+}
 ?>
 
 <!DOCTYPE html>
@@ -17,10 +35,11 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/connect.php";
 	<form action="process.php" method="post">
 
 		<label for="product_serie">Serie</label>
-		<input type="text" name="product_serie" id="product_serie" >
+		<input type="text" name="product_serie" id="product_serie" value="<?= hsc($product_serie)?>">
 		<label for="product_name">Titre</label>
-		<input type="text" name="product_name" id="product_name" >
+		<input type="text" name="product_name" id="product_name" value="<?= hsc($product_name)?>" >
 
+		<input type="hidden" name="product_id" value="<?= hsc($product_id)?>">
 		<input type="hidden" name="formCU" value="ok">
 		<input type="submit" value="Enregistrer">
 
--- admin/product/process.php
+++ admin/product/process.php
@@ -1,19 +1,36 @@
-<?php 
+<?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/function.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/protect.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/connect.php";
 
 
-if (isset($_POST["formCU"]) && $_POST["formCU"]=="ok"){
-      $stmt = $db -> prepare("INSERT INTO table_product(
-                                    product_serie,
-                                    product_name)
-                                    VALUES(
-                                    :product_serie,
-                                    :product_name
-                                    )");
-      $stmt -> bindValue(":product_serie",$_POST["product_serie"]);
-      $stmt -> bindValue(":product_name",$_POST["product_name"]);
-      $stmt -> execute();
+if (isset($_POST["formCU"]) && $_POST["formCU"] == "ok") {
+
+      // Ajout d'un champ dans la BDD
+      if ($_POST["product_id"] == 0) {
+            $stmt = $db->prepare("INSERT INTO table_product(
+            product_serie,
+            product_name)
+            VALUES(
+            :product_serie,
+            :product_name
+            )");
+            $stmt->bindValue(":product_serie", $_POST["product_serie"]);
+            $stmt->bindValue(":product_name", $_POST["product_name"]);
+            $stmt->execute();
+      } else {
+            // Modification d'un champ existant dans la base de données
+            $stmt = $db->prepare("UPDATE table_product SET
+            product_serie = :product_serie,
+            product_name = :product_name
+            WHERE 
+            product_id = :product_id");
+
+            $stmt->bindValue(":product_serie", $_POST["product_serie"]);
+            $stmt->bindValue(":product_name", $_POST["product_name"]);
+            $stmt->bindValue(":product_id", $_POST["product_id"]);
+            $stmt->execute();
+      }
 }
+
 redirect("index.php");
