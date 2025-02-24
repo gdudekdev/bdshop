8752d08f47c9f96aa986aec2e1e3ed758e10b257
gdudek <gdudek.dev@gmail.com>
2025-02-24 12:06:34 +0100

    chore/refactorisation de search.php

--- admin/product/search.php
+++ admin/product/search.php
@@ -6,8 +6,12 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/admin/include/protect.php";
 // setcookie('nom','valeur','temps en seconde, ex : 30*24*60*60')
 
 if (isset($_GET['reset']) && $_GET['reset'] == 1) {
-      setcookie('search_keyword',"",time()-3600);
-      unset($_COOKIE['search_keyword']);
+      foreach($_COOKIE as $key=>$value){
+            if(strpos($key,'search_')===0){
+                  setcookie($key,"",time()-3600);
+                  unset($_COOKIE[$key]);   
+            }
+      }
 } else {
       foreach ($_POST as $key => $value) {
             setcookie('search_' . $key, $value, time() + (30 * 24 * 60 * 60));
